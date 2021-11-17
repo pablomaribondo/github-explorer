800e0c58d2346d7b94446f8ad1bc509bc5fbacf7
Pablo Maribondo <pablomgvieira@gmail.com>
2021-11-17 17:09:11 -0300

    feat: update App component with new styles and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { FC } from "react";
-
-import RepositoryList from "./components/RepositoryList";
+import { Header } from "./components/Header";
+import { RepositoryInfo } from "./components/RepositoryInfo";
+import { RepositoryList } from "./components/RepositoryList";
 
 import "./styles/global.scss";
+import "./styles/app.scss";
 
-const App: FC = () => <RepositoryList />;
+const PROFILE = "rocketseat";
+
+const App = () => (
+  <div className="app">
+    <Header />
+    <RepositoryInfo profile={PROFILE} />
+    <RepositoryList profile={PROFILE} />
+  </div>
+);
 
 export default App;
